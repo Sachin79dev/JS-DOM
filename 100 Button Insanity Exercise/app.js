1b9d0b115921ae897f2e37967edbccbac35c61a4
Sachin Singh <ss0823903@gmail.com>
2023-10-09 23:35:51 +0530

    100 Button Insanity Exercise
    100 Button Insanity Exercise

---
+++ 100 Button Insanity Exercise/app.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+
+
+
+for (let i=0; i<100; i++) {
+    const btn = document.createElement('button')
+    btn.innerText ="Hey"
+    container.appendChild(btn);
+}
