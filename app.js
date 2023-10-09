f1bf642207b86e8cd95ffb325e331cc8b34122d6
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2023-10-09 20:55:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const box = document.querySelector("#container");
+
+box.style.textAlign="center";
+
+const image = document.querySelector("img");
+
+image.style.width="150px";
+image.style.borderRadius="50%";
