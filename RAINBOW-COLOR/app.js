71ecd3cda7dec9755c058df5e6ff392430a8f171
Sachin Singh <ss0823903@gmail.com>
2023-10-09 21:27:31 +0530

    Rainbow color
    rainbow color adding excercise

---
+++ RAINBOW-COLOR/app.js
@@ -0,0 +1,10 @@
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOU CODE GOES HERE:
+document.querySelectorAll('span')[0].style.color="red"
+document.querySelectorAll('span')[1].style.color="orange"
+document.querySelectorAll('span')[2].style.color="yellow"
+document.querySelectorAll('span')[3].style.color="green"
+document.querySelectorAll('span')[4].style.color="blue"
+document.querySelectorAll('span')[5].style.color="indigo"
+document.querySelectorAll('span')[6].style.color="violet"
